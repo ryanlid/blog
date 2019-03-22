08251c9400f5f271b5e6a94cb40ac7670910dd1b
ryan <ryanlid@live.com>
2019-03-22 20:49:08 +0800

    Site updated: 2019-03-22 20:49:08

--- downloads/code/useful-javascript-snippets/isFloat.js
+++ downloads/code/useful-javascript-snippets/isFloat.js
@@ -1,6 +1,6 @@
 // 判断字符串是否为浮点数
 function isFloat( str ) {
-  for(i=0;i<str.length;i++) {="" if="" ((str.charat(i)<"0"="" ||="" str.charat(i)="">"9")&& str.charAt(i) != '.'){
+  for(i=0;i<str.length;i++) { if ((str.charat(i)<"0" || str.charat(i)>"9")&& str.charAt(i) != '.'){
       return false;
     }
   }
